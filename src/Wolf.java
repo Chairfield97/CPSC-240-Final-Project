14ac11837b5953f69440a33757b0396732ee92af
tfritche <tfritche@mail.umw.edu>
2023-04-10 16:38:07 -0400

    added Wolf

---
+++ src/Wolf.java
@@ -0,0 +1,50 @@
+package src;
+
+import java.util.Random;
+
+public class Wolf implements Enemy{
+    private String type="Howling Wolf";
+    private int vitality=38;
+    @Override
+    public void attack(Player p, Random rng) {
+        if (p.damage(rng.nextInt(6,9), rng)) {
+            System.out.println("Wolf bite");
+        } else {
+            System.out.println(p.getName() + " dodged the wolf bite!");
+        }
+    }
+
+    @Override
+    public void specAttack(Player p, Random rng) {
+        if (p.damage(rng.nextInt(13,18),rng)) {
+            System.out.println("Slash and dash!!!");
+        } else {
+            System.out.println(p.getName() + " dodged  Slash and dash!");
+        }
+    }
+
+    @Override
+    public boolean damage(int dam, Random rng) {
+        if (rng.nextInt(0,4) == 1) {
+            vitality -= dam;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public void defend() {
+
+    }
+
+    @Override
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public int getVitality() {
+        return vitality;
+    }
+}
