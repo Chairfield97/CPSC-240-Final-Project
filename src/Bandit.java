650d546a7ad9914eb270030f2e195d36e70c60c6
tfritche <tfritche@mail.umw.edu>
2023-04-20 14:14:28 -0400

    added comments

--- src/Bandit.java
+++ src/Bandit.java
@@ -4,12 +4,12 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.Random;
-
+// inherited from enemy
 public class Bandit extends Enemy {
     private String type = "Bankrupt Bandit";
     private int vitality = 35;
     private final int maxVitality = getVitality();
-
+    //randomized the attack of bandit for attacks
     @Override
     public String attack(Player p, Random rng) {
         int damDealt = rng.nextInt(4,9);
@@ -19,7 +19,7 @@ public class Bandit extends Enemy {
             return(p.getName() + " dodged the sword jab!\n");
         }
     }
-
+    // randomized the attack of special attacks
     @Override
     public String specAttack(Player p, Random rng) {
         int damDealt = rng.nextInt(10,16);
@@ -29,7 +29,7 @@ public class Bandit extends Enemy {
             return(p.getName() + " dodged shield bash!\n");
         }
     }
-
+    // the chance they take damage
     @Override
     public boolean damage(int dam, Random rng) {
         if (rng.nextInt(0,4) != 1) {
@@ -39,16 +39,17 @@ public class Bandit extends Enemy {
             return false;
         }
     }
-
+    // return the type
     @Override
     public String getType() {
         return type;
     }
-
+    //returns the health
     @Override
     public int getVitality() {
         return vitality;
     }
+    //returns the max health
     public int getMaxVitality() {
         return maxVitality;
     }
