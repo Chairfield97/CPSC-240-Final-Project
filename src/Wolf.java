a00d26b4ff6e33f7c1f3d152468ee5e98511d0bf
tfritche <tfritche@mail.umw.edu>
2023-04-20 16:31:23 -0400

    added comments

--- src/Wolf.java
+++ src/Wolf.java
@@ -4,12 +4,12 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.Random;
-
+//inherited from enemy
 public class Wolf extends Enemy {
     private String type = "Ravenous Wolf";
     private int vitality = 30;
     private final int maxVitality = getVitality();
-
+    // // Enemy randomizer dealt damage attack for player
     @Override
     public String attack(Player p, Random rng) {
         int damDealt = rng.nextInt(4,9);
@@ -19,7 +19,7 @@ public class Wolf extends Enemy {
             return(p.getName() + " dodged the wolf bite!\n");
         }
     }
-
+    // Enemy randomizer dealt damage special attack for player
     @Override
     public String specAttack(Player p, Random rng) {
         int damDealt = rng.nextInt(10,16);
@@ -29,7 +29,7 @@ public class Wolf extends Enemy {
             return(p.getName() + " dodged Slash and dash!\n");
         }
     }
-
+    //Enemy randomizer to dodge player attacks
     @Override
     public boolean damage(int dam, Random rng) {
         if (rng.nextInt(0,2) != 1) {
@@ -39,16 +39,17 @@ public class Wolf extends Enemy {
             return false;
         }
     }
-
+    // return type
     @Override
     public String getType() {
         return type;
     }
-
+    // return health
     @Override
     public int getVitality() {
         return vitality;
     }
+    //return max health
     public int getMaxVitality() {
         return maxVitality;
     }
