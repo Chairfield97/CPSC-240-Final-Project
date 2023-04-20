e2c12c76fe2d4729e2e5de69c3e830da5b2dd448
tfritche <tfritche@mail.umw.edu>
2023-04-20 14:18:29 -0400

    added comments

--- src/AbstractEnemyFactory.java
+++ src/AbstractEnemyFactory.java
@@ -1,3 +1,5 @@
+//Was supposed to be used for events but we never used it
 public interface AbstractEnemyFactory {
+    // to create a new enemy for the event
     public Enemy createEnemy();
 }
--- src/ActionButton.java
+++ src/ActionButton.java
@@ -1,9 +1,10 @@
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-
+//Adding a button that never got used
 public class ActionButton implements ActionListener {
 
     private int num = 0;
+    //adding what it is supposed to be saying.
 
     @Override
     public void actionPerformed(ActionEvent e) {
