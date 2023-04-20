f948a9588b0886d2543011e5daf73aa8d5c6bdd9
tfritche <tfritche@mail.umw.edu>
2023-04-20 14:43:50 -0400

    added comments

--- src/BoarFactory.java
+++ src/BoarFactory.java
@@ -1,4 +1,6 @@
+// inherited from abstractEnemy factory
 public class BoarFactory implements AbstractEnemyFactory {
+  //Create a new boar enemy
     @Override
     public Enemy createEnemy() {
         return new Boar();
